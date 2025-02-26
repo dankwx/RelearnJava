64a18675a5aea9e064c61f45405dc5ce28a49219
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2025-02-26 01:42:25 -0300

    chore: updating ArrayLearning code

--- src/ArrayLearning.java
+++ src/ArrayLearning.java
@@ -1,16 +1,62 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ArrayLearning {
     public static void main(String[] args) {
+
+        boolean programActive = true;
+        int option = 0;
+        Scanner scanner = new Scanner(System.in);
+
         List<String> cores = new ArrayList<String>();
 
-        cores.add("Azul");
-        cores.add("Vermelho");
-        cores.add("Amarelo");
 
-        for (int i = 0; i < cores.size(); i++) {
-            System.out.println(cores.get(i));
+        while (programActive) {
+
+            System.out.println("O que deseja fazer?");
+            System.out.println("1. Listar filmes.");
+            System.out.println("2. Adicionar filme.");
+            System.out.println("3. Remover filme.");
+            System.out.println("4. Sair.");
+
+            if (scanner.hasNextInt()) {
+                option = scanner.nextInt();
+                scanner.nextLine();
+
+                if (option >= 1 && option <= 4) {
+                    System.out.println("Escolha aceita");
+                } else {
+                    System.out.println("Insira um número de 1 a 4");
+                }
+            } else {
+                System.out.println("Insira um número inteiro");
+                scanner.next();
+            }
+            switch (option) {
+                case 1:
+                    for (String cor : cores) {
+                        System.out.println(cor);
+                    }
+                    break;
+                case 2:
+                    System.out.println("Que filme deseja adicionar?");
+                    String filme = scanner.nextLine();
+                    cores.add(filme);
+                    break;
+                case 3:
+                    System.out.println("Escolheu remover filme");
+                    break;
+                case 4:
+                    System.out.println("Até mais!");
+                    programActive = false;
+                    break;
+                default:
+                    System.out.println("Escolha inválida");
+
+            }
+
         }
+
     }
 }
