fdc52ec6a1abbdc35ab81aaa88199a6e10f438a8
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2025-02-28 16:01:45 -0300

    faet

---
+++ src/TryCatchLearning.java
@@ -0,0 +1,19 @@
+public class TryCatchLearning {
+    public static void main(String[] args) {
+        boolean error = false;
+        try {
+            System.out.println(100 / 0);
+        } catch (Exception e) {
+            System.out.println("Erro:");
+            System.out.println(e.getMessage());
+            error = true;
+        } finally {
+            if (error) {
+                System.out.println("Favor tente novamente");
+            } else {
+                System.out.println("TUdo certo!");
+            }
+
+        }
+    }
+}
